7e202ed3345579e85aeaa20935c071ab3aaaa05f
Doan Quoc Bao <baodoan.ic@gmail.com>
2022-03-01 15:46:30 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# lab12_clubme_mobile
+
+A music platform.
+
+## Getting Started
+
+Application is under development.
---
+++ android/app/src/main/kotlin/com/lab12/lab12_clubme_mobile/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.lab12.lab12_clubme_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
